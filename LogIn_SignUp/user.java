8789b6d00a5d7a7cb614cd71bc9c6ae67aabbc1b
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-06-04 10:09:12 +0530

    Create user.java

---
+++ LogIn_SignUp/user.java
@@ -0,0 +1,18 @@
+package com.example.loginemailverification;
+
+public class User
+{
+    public  String fullname,age,email;
+
+    public User()
+    {
+
+    }
+
+    public User(String fullname,String age,String email)
+    {
+        this.fullname = fullname;
+        this.age = age;
+        this.email = email;
+    }
+}
